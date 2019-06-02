c3323ef0f4b54c5af8a2b3aeff70d4ba881bd87e
Lei Zhang <zhanglei@microduino.cc>
2019-06-02 13:57:34 +0800

    update
    update

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,12 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from recommonmark.parser import CommonMarkParser
 
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
 
 # -- Project information -----------------------------------------------------
 
@@ -60,3 +65,4 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
